7179ec0a73160f8019c94dfdf890bf2272ebef36
CrisChiaro <s294628@studenti.polito.it>
2023-11-06 12:11:08 +0100

    addedd routing service

--- quick start/tests/stop.sh
+++ quick start/tests/stop.sh
@@ -4,3 +4,4 @@ kubectl delete -f ../latency-aware-scheduler.yaml
 kubectl delete -f nginx-deployment.yaml
 kubectl delete -f lat-meas-serv.yaml
 kubectl delete -f ../routing-manager.yaml
+kubectl delete -f routing-service.yaml
